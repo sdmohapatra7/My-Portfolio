29c88c26cd27341960d6fa9c94902f125dd1f341
Shaktidhar Mohapatra <sdmohapatra7@gmail.com>
2025-07-27 13:32:58 +0530

    header and footer changes

--- src/App.js
+++ src/App.js
@@ -11,27 +11,39 @@ import "aos/dist/aos.css";
 import { ThemeContext } from "./context/Theme";
 function App() {
   const [{ themename }] = React.useContext(ThemeContext);
+
   React.useEffect(() => {
     Aos.init({ duration: 1500 });
   }, []);
+
   return (
     <div id="top" className={`${themename} app`}>
-      <section id="#home">
-        <Header />
-      </section>
+      {/* ✅ Header should NOT be inside a section */}
+      <Header />
+
       <main>
+        {/* Add id="home" here if needed for scroll target */}
+        <section id="home">
+          {/* You can add a hero or intro component here if needed */}
+        </section>
+
         <About />
-        <section id="#projects">
+
+        <section id="projects">
           <Projects />
         </section>
-        <section id="#contact">
+
+        <section id="contact">
           <Contact />
         </section>
       </main>
+
       <Footer />
       <ScrollToTop />
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Footer.css";
 import { ThemeContext } from "../../context/Theme";
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
 
 export const Footer = () => {
     const [{ themename }] = React.useContext(ThemeContext);
-
+    const [showNavList, setShowNavList] = React.useState(false);
+    const toggleNavList = (id) => {
+        var element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView();
+        }
+        setShowNavList(!showNavList);
+    };
     return (
         <footer className={`footer-wrapper section ${themename}`}>
             <div className="footer-grid">
@@ -21,21 +29,41 @@ export const Footer = () => {
                 <div className="footer-links">
                     <h4>Quick Links</h4>
                     <ul>
-                        <li><a href="#projects">Projects</a></li>
-                        <li><a href="#about">About Me</a></li>
-                        <li><a href="#skills">Skills</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#projects" onClick={() => toggleNavList("#projects")}
+                            >Projects</a></li>
+                        <li><a href="#about" onClick={() => toggleNavList("#about")}
+                            >About Me</a></li>
+                        <li><a href="#skills" onClick={() => toggleNavList("#skills")}
+                            >Skills</a></li>
+                        <li><a href="#contact" onClick={() => toggleNavList("#contact")}
+                            >Contact</a></li>
                     </ul>
                 </div>
 
-                {/* Social Media */}
+                {/* Social Media with Icons */}
                 <div className="footer-contact">
                     <h4>Connect</h4>
-                    <ul>
-                        <li><a href="https://www.linkedin.com/in/shaktidhar-mohapatra/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-                        <li><a href="https://github.com/sdmohapatra7" target="_blank" rel="noreferrer">GitHub</a></li>
-                        <li><a href="mailto:sdmohapatra7@gmail.com">Email</a></li>
-                        <li><a href="tel:+919777887204">📞 +91 9777887204</a></li>
+                    <ul className="connect-list">
+                        <li>
+                            <a href="https://www.linkedin.com/in/shaktidhar-mohapatra/" target="_blank" rel="noreferrer">
+                                <FaLinkedin /> LinkedIn
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/sdmohapatra7" target="_blank" rel="noreferrer">
+                                <FaGithub /> GitHub
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:sdmohapatra7@gmail.com">
+                                <FaEnvelope /> Email
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tel:+919777887204">
+                                <FaPhone /> +91 9777887204
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
